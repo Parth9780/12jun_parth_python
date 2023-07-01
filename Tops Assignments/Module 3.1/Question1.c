4a5d9547b5812431dec8e2043bd554e710b2e2c4
Parth9780 <136336448+Parth9780@users.noreply.github.com>
2023-07-01 17:24:20 +0530

    01-07-2023

---
+++ Tops Assignments/Module 3.1/Question1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("My name is Parth kavathiya\n");
+	printf("My birth date: 03-01-2005\n");
+	printf("My age is : 18 year\n");
+	printf("My adderess is 'Shapar (veraval), Gondal road, Rajkot'\n");
+}
