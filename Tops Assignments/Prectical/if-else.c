ece2ecb7e594340cfaea8ebdb034ee72dd7fbcb1
Parth9780 <136336448+Parth9780@users.noreply.github.com>
2023-07-08 18:19:26 +0530

    08/07/23

---
+++ Tops Assignments/Prectical/if-else.c
@@ -0,0 +1,21 @@
+/*
+    if-else is candition control 
+    if is ckeck the condition.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a, b;
+    char c;
+    printf("Enter the value of a: ");
+    scanf("%d",&a);
+    if (a >= 100){
+        printf("a is a three no value");
+    }
+    else{
+        printf("a is a two no value");
+    }
+    return 0;
+}
