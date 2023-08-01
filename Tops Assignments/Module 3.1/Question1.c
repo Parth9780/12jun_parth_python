02c61854431ee5fed10de19f62acccbc87cd05c6
Parth9780 <136336448+Parth9780@users.noreply.github.com>
2023-08-01 15:01:18 +0530

    Delete Tops Assignments/Module 3.1 directory

--- Tops Assignments/Module 3.1/Question1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main()
-{
-	printf("My name is Parth kavathiya\n");
-	printf("My birth date: 03-01-2005\n");
-	printf("My age is : 18 year\n");
-	printf("My adderess is 'Shapar (veraval), Gondal road, Rajkot'\n");
-}
