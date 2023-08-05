ec3428b66ff2453ddec91073d307da2c9a33da50
Parth9780 <136336448+Parth9780@users.noreply.github.com>
2023-08-05 17:38:11 +0530

    05-08-2023

---
+++ Tops Assignments/Prectical/matrix.c
@@ -0,0 +1,39 @@
+// adition of 2x2 matrix
+
+#include<stdio.h>
+int main()
+{
+    int m1[2][2];
+    int m2[2][2];
+    for(int i; i<2; i++)
+    {
+       for(int j; j<2; j++)
+       {
+            printf("Enter the element: ");
+            scanf("%d",&m1[i][j]);
+       }
+    }
+    for(int i; i<2; i++)
+    {
+       for(int j; j<2; j++)
+       {
+            printf("%d ",m1[i][j]);
+       }
+    }
+    for(int i; i<2; i++)
+    {
+       for(int j; j<2; j++)
+       {
+            printf("Enter the element: ");
+            scanf("%d",&m2[i][j]);
+       }
+    }
+    for(int i; i<2; i++)
+    {
+       for(int j; j<2; j++)
+       {
+            printf("%d ",m2[i][j]);
+       }
+    }
+    return 0;
+}
